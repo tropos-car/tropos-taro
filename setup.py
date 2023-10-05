afcb4e33521ecf009e3218e69f45c5ec9474d03a
Jonas Witthuhn <witthuhn@tropos.de>
2023-10-05 11:34:38 +0200

    add package_data *.CR1X

--- setup.py
+++ setup.py
@@ -1,44 +1,43 @@
-
 from setuptools import setup
 import versioneer
 
 setup(
-        name = "mordor",
-        version = versioneer.get_version(),
-        cmdclass = versioneer.get_cmdclass(),
-        author = "Jonas Witthuhn",
-        author_email = "witthuhn@tropos.de",
-        license = "OSI Approved :: GNU General Public License v3 (GPLv3)",
-        packages = ["mordor"],
-        package_dir = {"":"src"},
-        package_data = {"mordor": ["share/*.json"]},
-        include_package_data = True,
-        entry_points = {
-            "console_scripts": [
-                "mordor = mordor.click:cli",
-                ],
-            },
-        install_requires = [
-            "numpy",
-            "xarray",
-            "netcdf4",
-            "pip",
-            "jstyleson",
-            "Click",
-            "toolz",
-            "addict",
-            "trosat-base @ git+https://github.com/hdeneke/trosat-base.git#egg=trosat-base",
-            ],
-        extras_require = {
-            "docs": [
-                "sphinx",
-                "myst-parser",
-                "myst-nb",
-                ],
-            "nbs": [
-                "jupyter",
-                "nbdev",
-                "nbformat",
-                ],
-            },
-        )
+    name="mordor",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Jonas Witthuhn",
+    author_email="witthuhn@tropos.de",
+    license="OSI Approved :: GNU General Public License v3 (GPLv3)",
+    packages=["mordor"],
+    package_dir={"": "src"},
+    package_data={"mordor": ["share/*.json", "share/*.CR1X"]},
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "mordor = mordor.click:cli",
+        ],
+    },
+    install_requires=[
+        "numpy",
+        "xarray",
+        "netcdf4",
+        "pip",
+        "jstyleson",
+        "Click",
+        "toolz",
+        "addict",
+        "trosat-base @ git+https://github.com/hdeneke/trosat-base.git#egg=trosat-base",
+    ],
+    extras_require={
+        "docs": [
+            "sphinx",
+            "myst-parser",
+            "myst-nb",
+        ],
+        "nbs": [
+            "jupyter",
+            "nbdev",
+            "nbformat",
+        ],
+    },
+)
