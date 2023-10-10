f953b2c5e1cbee39572d4c71fd457baff2b8fad6
Jonas Witthuhn <witthuhn@tropos.de>
2023-10-10 10:06:00 +0200

    add locationName argument and reformat destination filename

--- bin/wiser_move.bat
+++ bin/wiser_move.bat
@@ -1,11 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Get source folder and destination root folder from command-line arguments
-set "sourceFolder=%~1"
-set "destinationRoot=%~2"
+:: Get location name, source folder and destination root folder from command-line arguments
+set "locationName=%~1"
+set "sourceFolder=%~2"
+set "destinationRoot=%~3"
 
-:: Check if both source and destination folders are provided
+:: Check if location name, source and destination folders are provided
+if "%locationName%"=="" (
+    echo Location name is missing.
+    exit /b 1
+)
+
 if "%sourceFolder%"=="" (
     echo Source folder path is missing.
     exit /b 1
@@ -27,14 +33,15 @@ for %%F in (%sourceFolder%\%currentDate:~0,4%\*.WSD) do (
     set "fileDate=!fileName:~0,8!"
     set "year=!fileDate:~0,4!"
     set "month=!fileDate:~4,2!"
+    set "day=!fileDate:~6,2!"
     
     if "!fileDate!" neq "!currentDate:~0,8!" (
-        set "destinationFolder=%destinationRoot%\!year!\!month!"
+        set "destinationFolder=%destinationRoot%\!year!\!month!\!day!"
         if not exist "!destinationFolder!" (
             mkdir "!destinationFolder!"
         )
-        
-        move "%%F" "!destinationFolder!\!fileName!.WSD"
+        set "newFileName=wiser_!year!-!month!-!day!_!locationName!.WSD"
+        move "%%F" "!destinationFolder!\!newFileName!"
     )
 )
 
@@ -44,13 +51,14 @@ for %%F in (%sourceFolder%\%currentDate:~0,4%\*.CSV) do (
     set "year=!fileDate:~0,4!"
     set "month=!fileDate:~4,2!"
     set "day=!fileDate:~6,2!"
+    set "hour"=!fileDate:~8,2!"
     
     if "!fileDate!" neq "!currentDate!" (
         set "destinationFolder=%destinationRoot%\!year!\!month!\!day!"
         if not exist "!destinationFolder!" (
             mkdir "!destinationFolder!"
         )
-    
+        set "newFileName=wiser_!year!-!month!-!day!T!hour!_!locationName!.CSV"
         move "%%F" "!destinationFolder!\!fileName!.CSV"
     )
 )
