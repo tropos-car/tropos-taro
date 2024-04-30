c8850df72b60783a198133db9c523eb13a4b7597
Jonas Witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2024-04-30 10:08:16 +0200

    asi16 move use replace instead of rename

--- src/mordor/click.py
+++ src/mordor/click.py
@@ -484,7 +484,7 @@ def asi16_move(
             **config
         )
         os.makedirs(path_out, exist_ok=True)
-        os.rename(fn, os.path.join(path_out, fname_out))
+        os.replace(fn, os.path.join(path_out, fname_out))
 
 def asi16_missing_dates(
         images_path: str,
@@ -600,7 +600,7 @@ def asi16_move_unprocessed(
             **config
         )
         os.makedirs(path_out, exist_ok=True)
-        os.rename(fn, os.path.join(path_out, fname_out))
+        os.replace(fn, os.path.join(path_out, fname_out))
 
 @cli_asi16.command("move-processed")
 @click.argument("images_path", nargs=1)
@@ -656,8 +656,8 @@ def asi16_move_processed(
         )
         os.makedirs(path_out, exist_ok=True)
         os.makedirs(path_out_pro, exist_ok=True)
-        os.rename(fn, os.path.join(path_out, fname_out))
-        os.rename(fnp, os.path.join(path_out_pro, fname_out_pro))
+        os.replace(fn, os.path.join(path_out, fname_out))
+        os.replace(fnp, os.path.join(path_out_pro, fname_out_pro))
 
 
 @cli_asi16.command("keogram")
