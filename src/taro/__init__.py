fd7abb1fa689df1713142a2c935a0feb44f5b84f
Jonas Witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2025-03-06 16:33:16 +0100

    Update __init__.py

--- src/taro/__init__.py
+++ src/taro/__init__.py
@@ -1,7 +1,7 @@
 
 from importlib.metadata import version, PackageNotFoundError
 try:
-    __version__ = version("starpas")
+    __version__ = version("taro")
 except PackageNotFoundError:
     # package is not installed
     pass
